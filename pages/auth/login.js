aa92851e2b7b4f89d1330e2544b174e584ca95f5
Seno PC <senosetiaji21@gmail.com>
2026-04-03 21:02:16 +0700

    feat(auth): add redirectAuthenticatedUser helper for session-based redirection

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,12 +1,18 @@
 import Login from '@/containers/auth'
 import React from 'react'
+import { redirectAuthenticatedUser } from '@/lib/helpers/redirectAuthenticatedUser'
 
-function Index() {
+/** @param {import('next').InferGetServerSidePropsType<typeof getServerSideProps> } props */
+function Index(props) {
   return (
     <div>
         <Login />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  return redirectAuthenticatedUser(context, '/dashboard');
 }
 
 export default Index
